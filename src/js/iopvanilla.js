ecdfe7a55575295019ce329e4f7e4a5c3cc54105
David Snodgrass <david.snodgrass@supercreative.us>
2020-09-18 16:07:17 -0400

    Fixes for Nav, added hover states
    Fixes on nav animation
    Additional hover states on upper right block

--- src/js/iopvanilla.js
+++ src/js/iopvanilla.js
@@ -2,11 +2,16 @@ function navToggle() {
   var element = document.getElementById("navblock");
   element.classList.toggle("hidenav");
   element.classList.toggle("shownav");
+  setTimeout(() => {  element.classList.toggle("hideme"); }, 250);
 }
 
 window.addEventListener("resize", function() {
-if (window.innerWidth > 992)
+if (window.innerWidth > 992){
+if (document.getElementById("navblock") !== 'undefined'){
 var element = document.getElementById("navblock");
+
 element.classList.remove("shownav");
 element.classList.add("hidenav");
+element.classList.add("hideme");
+}}
 });
