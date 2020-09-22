5f8cf953930361ba8be764dbceda962c9206777c
David Snodgrass <david.snodgrass@supercreative.us>
2020-09-22 13:48:41 -0400

    Merged with Jesse CSS
    Merged with Jesse CSS & updates to login

--- src/js/iopvanilla.js
+++ src/js/iopvanilla.js
@@ -15,3 +15,32 @@ element.classList.add("hidenav");
 element.classList.add("hideme");
 }}
 });
+
+
+function tabs(tabber, tabName, itemid){
+  let tabs = document.getElementById(tabber);
+  let children = tabs.children;
+  for (var i=0; i<children.length; i++) {
+    children[i].classList.remove('active');
+  }
+  document.getElementById(itemid).classList.add('active');
+  var tabcontentID = tabber + "content";
+
+  let tabcontent = document.getElementById(tabcontentID);
+  let contentchildren = tabcontent.children;
+  for (var i=0; i<children.length; i++) {
+    contentchildren[i].classList.remove('active');
+  }
+  document.getElementById(tabName).classList.add('active');
+}
+
+/* Tabs
+$(".card").swipe( {
+     swipeLeft:function(event, direction, distance, duration, fingerCount) {
+          $(".nav-tabs li.active").next('li').find('a').tab('show');
+        },
+     swipeRight:function(event, direction, distance, duration, fingerCount) {
+          $(".nav-tabs li.active").prev('li').find('a').tab('show');
+        },
+  });
+*/
